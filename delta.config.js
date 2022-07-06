1882caebae1eddc05ee908ad8f2d5cb2952ebecb
Daniel da Silva <danielfdsilva@gmail.com>
2022-07-06 12:43:41 +0100

    Add about page overrides

--- delta.config.js
+++ delta.config.js
@@ -12,5 +12,14 @@ module.exports = {
   /**
    * Glob path for the discoveries.
    */
-  discoveries: './discoveries/*.discoveries.mdx'
+  discoveries: './discoveries/*.discoveries.mdx',
+
+  // App component and content overrides.
+  // See docs/CONFIGURATION.md for more information.
+  pageOverrides: {
+    // Content for the root about page. This is the global about page, before
+    // entering a thematic area.
+    // Type: Content override
+    aboutContent: './overrides/about.mdx'
+  }
 };
