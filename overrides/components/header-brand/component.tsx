24bcbdddb1483e3559e411292e62bf8b22ad4a0f
Daniel da Silva <danielfdsilva@gmail.com>
2023-06-15 16:25:25 +0100

    Add ghg logo

--- overrides/components/header-brand/component.tsx
+++ overrides/components/header-brand/component.tsx
@@ -1,20 +1,19 @@
-import React from '$veda-ui/react';
-import styled from '$veda-ui/styled-components';
-import { glsp, themeVal } from '$veda-ui/@devseed-ui/theme-provider';
-import { Link } from '$veda-ui/react-router-dom';
+import React from "$veda-ui/react";
+import styled from "$veda-ui/styled-components";
+import { glsp, themeVal } from "$veda-ui/@devseed-ui/theme-provider";
+import { Link } from "$veda-ui/react-router-dom";
+
+import ghgLogo from "./ghg-logo.png";
 
 const Brand = styled.div`
   display: flex;
   align-items: center;
   gap: ${glsp()};
 
-  .icon {
-    font-size: 2rem;
-  }
-
   a {
-    display: grid;
+    display: flex;
     align-items: center;
+    gap: ${glsp(0.5)};
 
     &,
     &:visited {
@@ -25,16 +24,21 @@ const Brand = styled.div`
     span {
       font-size: 1.25rem;
       line-height: 1.5rem;
-      font-weight: ${themeVal('type.base.regular')};
+      font-weight: ${themeVal("type.base.regular")};
       letter-spacing: -0.025em;
     }
+  }
+
+  img {
+    width: 3rem;
   }
 `;
 
 export default function HeaderComponent() {
   return (
     <Brand>
-      <Link to='/'>
+      <Link to="/">
+        <img src={ghgLogo} />
         <span>GHG Center</span>
       </Link>
     </Brand>
