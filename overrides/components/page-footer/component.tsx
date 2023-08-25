27563af6f77f1da6ccabe584309d402ccc70a3c1
Daniel da Silva <danielfdsilva@gmail.com>
2023-08-25 18:46:11 +0100

    Update footer

--- overrides/components/page-footer/component.tsx
+++ overrides/components/page-footer/component.tsx
@@ -1,24 +1,132 @@
 import React from "$veda-ui/react";
+import styled from "$veda-ui/styled-components";
+import { NavLink } from "$veda-ui/react-router-dom";
+import {
+  themeVal,
+  glsp,
+  listReset,
+  media,
+} from "$veda-ui/@devseed-ui/theme-provider";
 import { format } from "$veda-ui/date-fns";
+import { getString } from "veda";
+
 import { Tip } from "$veda-ui-scripts/components/common/tip";
+import { variableGlsp } from "$veda-ui-scripts/styles/variable-utils";
+import {
+  STORIES_PATH,
+  DATASETS_PATH,
+  ANALYSIS_PATH,
+  ABOUT_PATH,
+} from "$veda-ui-scripts/utils/routes";
+import { useFeedbackModal } from "$veda-ui-scripts/components/common/layout-root";
+
+const FooterInner = styled.div`
+  display: flex;
+  flex-flow: column;
+  flex-grow: 1;
+  width: 100%;
+  margin: ${variableGlsp(-0.75, -1)};
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: inset 0 -1px 0 0 ${themeVal("color.base-100a")};
+  padding: ${variableGlsp(1.5, 1)};
+`;
+
+const FooterNav = styled.nav``;
+
+const FooterBrand = styled.p`
+  font-weight: ${themeVal("type.base.bold")};
+`;
+
+const Credits = styled.div`
+  padding: ${variableGlsp(0.75, 1)};
+`;
+
+const FooterMenu = styled.ul`
+  ${listReset()}
+  display: flex;
+  flex-flow: column nowrap;
+  gap: ${glsp(0.5)};
+
+  ${media.largeUp`
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    gap: ${glsp(3)};
+  `}
+`;
+
+const FooterMenuLink = styled(NavLink)`
+  font-weight: ${themeVal("type.base.regular")};
+  text-decoration: none;
+  font-size: 0.875rem;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export default function PageFooter(props) {
   const nowDate = new Date();
 
+  const { show } = useFeedbackModal();
+
   return (
-    <p>
-      <a href="https://earthdata.nasa.gov/">
-        <span>By</span> <strong>U.S. GHG Center</strong> <span>on</span>{" "}
-        <time dateTime={String(nowDate.getFullYear())}>
-          {nowDate.getFullYear()}
-        </time>
-      </a>
-      {" • "}
-      <Tip
-        content={`Released on ${format(new Date(+props.appBuildTime), "PPPP")}`}
-      >
-        <span>v{props.appVersion}</span>
-      </Tip>
-    </p>
+    <FooterInner>
+      <FooterContent>
+        <FooterBrand>U.S. GHG Center</FooterBrand>
+        <FooterNav>
+          <FooterMenu>
+            <li>
+              <FooterMenuLink to={DATASETS_PATH}>Data Catalog</FooterMenuLink>
+            </li>
+            <li>
+              <FooterMenuLink to={ANALYSIS_PATH}>Analysis</FooterMenuLink>
+            </li>
+            <li>
+              <FooterMenuLink to={STORIES_PATH}>
+                {getString("stories").other}
+              </FooterMenuLink>
+            </li>
+            <li>
+              <FooterMenuLink to={ABOUT_PATH}>About</FooterMenuLink>
+            </li>
+            <li>
+              <FooterMenuLink
+                as="a"
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  show();
+                }}
+              >
+                Give Feedback
+              </FooterMenuLink>
+            </li>
+          </FooterMenu>
+        </FooterNav>
+      </FooterContent>
+      <Credits>
+        <a href="https://earthdata.nasa.gov/">
+          <span>By</span> <strong>U.S. GHG Center</strong> <span>on</span>{" "}
+          <time dateTime={String(nowDate.getFullYear())}>
+            {nowDate.getFullYear()}
+          </time>
+        </a>
+        {" • "}
+        <Tip
+          content={`Released on ${format(
+            new Date(+props.appBuildTime),
+            "PPPP"
+          )} (veda-ui v${props.appUiVersion})`}
+        >
+          <span>v{props.appVersion}</span>
+        </Tip>
+      </Credits>
+    </FooterInner>
   );
 }
