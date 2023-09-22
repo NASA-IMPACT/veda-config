d70bab7456707b9943e7f1fd39182b0d5f0d1242
Jonas <j08lue@gmail.com>
2023-09-22 18:22:23 +0200

    Change title and subtitle on development page (#147)
    * Change title and subtitle on development page
    
    * Move version reference to separate paragraph
    
    * Update development page text based on feedback
    
    ---------
    
    Co-authored-by: Slesa Adhikari <slesaad@gmail.com>

--- overrides/development/component.tsx
+++ overrides/development/component.tsx
@@ -20,7 +20,7 @@ export default function Component() {
   return (
     <FoldProse>
       <p>
-      The current US GHG Center platform reflects an initial (v{process.env.APP_VERSION}) release available as of October 2nd, 2023. Newly added data, feature changes and content updates will continue via an iterative process throughout the coming year.  
+      The current US GHG Center platform reflects an initial (v{process.env.APP_VERSION}) release available October 2nd, 2023. Newly added data, feature changes and content updates will continue to be made via an iterative process throughout the coming year.
       </p>
       <p>
       We welcome your feedback to help improve the US GHG Center! To share your thoughts, please click on the Contact Us button below or use the button available at the top of any of the Center website pages.
