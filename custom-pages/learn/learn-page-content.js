a1b9d3b5860535166947af0cddf415d195f95560
Slesa Adhikari <slesaad@gmail.com>
2024-01-11 08:58:45 -0600

    Update summer school date

--- custom-pages/learn/learn-page-content.js
+++ custom-pages/learn/learn-page-content.js
@@ -58,8 +58,8 @@ export const EVENT_ITEMS = [
       alt: 'Summer school info screenshot'
     },
     description: 'The US GHG Center is hosting a Summer School for Inverse Modeling of Greenhouse Gases (SSIM-GHG), June 11-21, 2024 at Fort Collins, CO, USA. The goal of the workshop is to present and provide guidance and instruction of the state of the art in atmospheric data assimilation techniques needed to support current and future GHG observing systems.',
-    startDate: '2023-06-11',
-    endDate: '2023-06-21'
+    startDate: '2024-06-11',
+    endDate: '2024-06-21'
   },
   {
     name: 'Launch of the US Greenhouse Gas Center at COP28 (12-4-23)',
