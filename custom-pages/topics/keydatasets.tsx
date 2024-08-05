5b0a854c502e61675cbb2f0aa43362cc2a5ec0cc
Brian Freitag <freitagb@users.noreply.github.com>
2024-08-05 11:26:08 -0500

    Update keydatasets.tsx

--- custom-pages/topics/keydatasets.tsx
+++ custom-pages/topics/keydatasets.tsx
@@ -49,71 +49,38 @@ const KeypointCard = styled(Card)`
   `}
 `
 
-export function KeypointsDatasets() {
+export default function Keypoints() {
   return (
     <KeypointsWrapper>
       <CardList>
         <li>
           <KeypointCard
             linkLabel="View more"
-            linkTo=""
-            title="Dataset 1"
-            description="Dataset 1"
-            imgSrc={"https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"}
+            linkTo="/stories/us-methane-sources"
+            title="Gridded Anthropogenic Greenhouse Gas Emissions"
+            description="Emission estimates from human activities including the energy, agriculture, waste, and industry sectors"
+            imgSrc={featureEmissions}
             imgAlt="image of oil refinery"
-            // footerContent={
-            //   <div>
-            //     <ArrowLink
-            //       to={`${DATASETS_PATH}?${Actions.TAXONOMY}=${encodeURIComponent(
-            //         JSON.stringify({ Topics: "anthropogenic-emissions" })
-            //       )}`}
-            //     >
-            //       Check out relevant datasets
-            //     </ArrowLink>
-            //   </div>
-            // }
           />
         </li>
         <li>
           <KeypointCard
             linkLabel="View more"
-            linkTo=""
-            title="Dataset 2"
-            description="Dataset2"
-            imgSrc={"https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"}
+            linkTo="/stories/tracking-greenhouse-gas-cycles"
+            title="Natural Greenhouse Gas Sources and Sinks"
+            description="Naturally-occurring greenhouse gas fluxes from land, ocean, and atmosphere"
+            imgSrc={featureSources}
             imgAlt="image of green wetlands"
-            // footerContent={
-            //   <div>
-            //     <ArrowLink
-            //       to={`${DATASETS_PATH}?${Actions.TAXONOMY}=${encodeURIComponent(
-            //         JSON.stringify({ Topics: "natural-emissions-and-sinks" })
-            //       )}`}
-            //     >
-            //       Check out relevant datasets
-            //     </ArrowLink>
-            //   </div>
-            // }
           />
         </li>
         <li>
           <KeypointCard
             linkLabel="View more"
-            linkTo=""
-            title="Dataset 3"
-            description="Dataset 3"
-            imgSrc={"https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"}
+            linkTo="/stories/discovering-large-methane-emissions"
+            title="New Observations for Tracking Large Emission Events"
+            description="Identify and quantify large methane leak events leveraging aircraft and space-based data"
+            imgSrc={featureMethane}
             imgAlt="image of colorful polygon against satellite landscape surface in brown"
-            // footerContent={
-            //   <div>
-            //     <ArrowLink
-            //       to={`${DATASETS_PATH}?${Actions.TAXONOMY}=${encodeURIComponent(
-            //         JSON.stringify({ Topics: "large-emission-events" })
-            //         )}`}
-            //         >
-            //       Check out relevant datasets
-            //     </ArrowLink>
-            //   </div>
-            // }
           />
         </li>
       </CardList>
