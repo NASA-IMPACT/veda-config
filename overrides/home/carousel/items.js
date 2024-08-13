8517c17a0be87bbb844bde0597f9f32844a70bbc
Slesa Adhikari <slesaad@gmail.com>
2024-08-13 16:56:11 -0500

    Add descriptions for carousel items

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -1,17 +1,17 @@
 export default [{
   link: '/stories/urban',
   title: 'Urban',
-  description: 'Description for the item 1 - internal link - make it a littttttle bit lonnnnggggger',
+  description: 'The U.S. Greenhouse Gas Center welcomes a new set of tools aimed at supporting city decisionmakers.',
   image: 'https://plus.unsplash.com/premium_photo-1680284197425-4bd125d75cab?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 }, {
   link: '/stories/graapes',
   title: 'GRAAPES',
-  description: 'Description for the item 2',
-  image: 'https://www.nist.gov/sites/default/files/styles/480_x_480_limit/public/images/2023/03/22/GRAAPES_2017_CO2.png?itok=T_IOom0Z'
+  description: "Greenhouse Gas And Air Pollutants Emissions System (GRAAPES) is a joint activity between NOAA Chemical Science Laboratory (NOAA CSL) and NIST's Greenhouse Gas (GHG) Measurement Program.",
+  image: 'https://i.imgur.com/1l4sw3z_d.webp?maxwidth=760&fidelity=grand'
 },
 {
   link: '/stories/newyork',
   title: 'New York City',
-  description: 'Description for the item 3',
+  description: 'See how New York City is developing policies to reduce the Greenhouse Gas emitting activities.',
   image: 'https://i.natgeofe.com/n/874df281-d3e0-489a-98c0-6b840023b828/newyork_NationalGeographic_2328428.jpg'
 }]
