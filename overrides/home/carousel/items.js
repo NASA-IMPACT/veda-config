b7834b6066c6166f7b4a27ff29059e7114d10ffb
Slesa Adhikari <slesaad@gmail.com>
2024-08-28 16:09:33 -0500

    GRAAPES to GRA2PES

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -4,9 +4,9 @@ export default [{
   description: 'The U.S. Greenhouse Gas Center welcomes a new set of tools aimed at supporting city decisionmakers.',
   image: 'https://plus.unsplash.com/premium_photo-1680284197425-4bd125d75cab?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 }, {
-  link: '/stories/graapes',
-  title: 'GRAAPES',
-  description: "Greenhouse Gas And Air Pollutants Emissions System (GRAAPES) is a joint activity between NOAA Chemical Science Laboratory (NOAA CSL) and NIST's Greenhouse Gas (GHG) Measurement Program.",
+  link: '/stories/gra2pes',
+  title: 'GRA2PES',
+  description: "Greenhouse Gas And Air Pollutants Emissions System (GRA2PES) is a joint activity between NOAA Chemical Science Laboratory (NOAA CSL) and NIST's Greenhouse Gas (GHG) Measurement Program.",
   image: 'https://i.imgur.com/1l4sw3z_d.webp?maxwidth=760&fidelity=grand'
 },
 {
