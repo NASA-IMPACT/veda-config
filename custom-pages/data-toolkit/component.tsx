14b9acc6107fcb04a3df4850fac5f6b0c543a0e8
Hanbyul Jo <hanbyul@nyu.edu>
2024-08-29 09:23:52 -0400

    Make Grid responsive, put icons in grid

--- custom-pages/data-toolkit/component.tsx
+++ custom-pages/data-toolkit/component.tsx
@@ -45,20 +45,32 @@ export default function Component() {
                 <Keypoints data={focusAreasSansDatasets} />
                 </section>
 
-                <Grid col={8} offset={2}>
+                <Grid tablet={{col:8, offset: 2}}>
                     <Title> Learn More and Share Your Ideas </Title>
-                    <div className="display-flex flex-align-center margin-bottom-2">
-                        <CollecticonEnvelope size='xlarge' color='#082a64' className="grid-col-1" />
-                        <span className="margin-left-4 font-sans-md"> For the latest updates and information about the US GHG Center or to join a focus area group, <a href={SUBSCRIPTION_URL}>subscribe to our email updates list.</a></span>
-                    </div>
-                    <div className="display-flex flex-align-center margin-bottom-2">
-                        <CollecticonTextBlock size='xlarge' color='#082a64' className="grid-col-1" />
-                        <span className="margin-left-4 font-sans-md"> Read more about the U.S. Greenhouse Gas Center news, trainings, and workshop opportunities on the <Link to="/news-and-events">News and Events page.</Link></span>
-                    </div>
-                    <div className="display-flex flex-align-center margin-bottom-2">
-                        <CollecticonSpeechBalloon size='xlarge' color='#082a64' className="grid-col-1" />
-                        <span className="margin-left-4 font-sans-md">  Do you have a US GHG portal suggestion or question? Reach the team using the "Contact Us" button at the top or bottom of every page.</span>
-                    </div>
+                    <Grid row>
+                        <Grid col='auto' className="display-flex flex-align-center margin-bottom-2">
+                            <CollecticonEnvelope size='xlarge' color='#082a64' className="grid-col-auto" />
+                        </Grid>
+                        <Grid col='fill' className="display-flex flex-align-center margin-bottom-2">
+                            <span className="margin-left-4 font-sans-md"> For the latest updates and information about the US GHG Center or to join a focus area group, <a href={SUBSCRIPTION_URL}>subscribe to our email updates list.</a></span>
+                        </Grid>
+                    </Grid>
+                    <Grid row>
+                        <Grid col='auto' className="display-flex flex-align-center margin-bottom-2">
+                            <CollecticonTextBlock size='xlarge' color='#082a64' className="grid-col-auto" />
+                        </Grid>
+                        <Grid col='fill' className="display-flex flex-align-center margin-bottom-2">
+                            <span className="margin-left-4 font-sans-md"> Read more about the U.S. Greenhouse Gas Center news, trainings, and workshop opportunities on the <Link to="/news-and-events">News and Events page.</Link></span>
+                        </Grid>
+                    </Grid>
+                    <Grid row>
+                        <Grid col='auto' className="display-flex flex-align-center margin-bottom-2">
+                            <CollecticonSpeechBalloon size='xlarge' color='#082a64' className="grid-col-auto" />
+                        </Grid>
+                        <Grid col='fill' className="display-flex flex-align-center margin-bottom-2">
+                            <span className="margin-left-4 font-sans-md">  Do you have a US GHG portal suggestion or question? Reach the team using the "Contact Us" button at the top or bottom of every page.</span>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </GridContainer>
         </div>
