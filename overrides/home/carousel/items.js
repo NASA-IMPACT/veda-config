962978ca1372b0bf197fb73d864781ec77449bd9
Slesa Adhikari <slesaad@gmail.com>
2024-09-17 14:46:07 -0500

    Updates

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -1,15 +1,15 @@
 export default [{
   link: '/stories/urban',
   title: 'Spotlight on Urban Emissions',
-  description: 'The U.S. Greenhouse Gas Center offers an introduction to new urban-relevant datasets and highlights innovative emissions research in cities across the country.',
+  description: 'The U.S. Greenhouse Gas Center offers an introduction to the latest national and city scale datasets and highlights innovative emissions research in cities across the country.',
   image: 'https://www.nasa.gov/wp-content/uploads/2023/03/1-maia-los-angeles-1041.jpg',
   imageAlt: 'Los Angeles skyline shown with visible haze of air pollution.'
 }, {
   link: '/stories/gra2pes',
   title: 'Nationwide Dataset Connects Greenhouse Gases and Air Quality',
-  description: "The GReenhouse gas And Air Pollutants Emissions System (GRA²PES) is the first U.S.-based measurement system to combine greenhouse gas and air quality pollutant sources.",
+  description: "The GReenhouse gas And Air Pollutants Emissions System (GRA²PES) is a new measurement system to combine greenhouse gas and air quality pollutant sources.",
   image: new URL('../../media/gra2pes.png', import.meta.url).href,
-  imageAlt: ''
+  imageAlt: 'two GRA2PES data plots: left is gridded CO2 emissions and right is NOx emissions over the Northeast US.'
 },
 {
   link: '/stories/newyork',
