bbc4133ed2f2dce7ed74ec8f65f673c7cec89376
snmln <smalone@element84.com>
2024-10-30 15:59:16 -0400

    turning on cookie consent form

--- veda.config.js
+++ veda.config.js
@@ -22,5 +22,9 @@ module.exports = {
       one: ' Data Story',
       other: 'Data Stories'
     }
+  },
+  cookieConsentForm: {
+    title: 'Cookie Consent',
+    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec nibh nec elit condimentum scelerisque sed vel massa. Fusce non massa eu justo rutrum euismod vitae sit amet libero. To learn more about it, see our [Privacy Policy](https://www.nasa.gov/privacy/#cookies)'
   }
 };
