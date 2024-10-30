f5e35e849db7618f4309bc13096829c5dfa6016e
snmln <smalone@element84.com>
2024-10-30 16:42:43 -0400

    turning on cookie consent form

--- veda.config.js
+++ veda.config.js
@@ -2,25 +2,29 @@ module.exports = {
   /**
    * Glob path for the datasets.
    */
-  datasets: './datasets/*.data.mdx',
+  datasets: "./datasets/*.data.mdx",
 
   /**
    * Glob path for the stories.
    */
-  stories: './stories/*.stories.mdx',
+  stories: "./stories/*.stories.mdx",
 
   // App component and content overrides.
   // See docs/CONFIGURATION.md for more information.
   pageOverrides: {
     // Content for the about page.
     // Type: Content override
-    aboutContent: './overrides/about.mdx'
+    aboutContent: "./overrides/about.mdx",
   },
 
   strings: {
     stories: {
-      one: ' Data Story',
-      other: 'Data Stories'
-    }
-  }
+      one: " Data Story",
+      other: "Data Stories",
+    },
+  },
+  cookieConsentForm: {
+    title: "Cookie Consent",
+    copy: "We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our [Privacy Policy](https://www.nasa.gov/privacy/#cookies)",
+  },
 };
