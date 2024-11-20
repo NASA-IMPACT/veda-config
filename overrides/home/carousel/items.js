dfe828a9c8f1b27f9a954381e3715915c8331775
Slesa Adhikari <slesaad@gmail.com>
2024-11-20 15:55:18 -0600

    Add methane spotlight to carousel

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -1,7 +1,14 @@
 export default [
   {
+    link: '/stories/methane',
+    title: 'Spotlight on Methane Emissions',
+    description: 'The U.S. Greenhouse Gas Center...',
+    image: new URL('../../media/Methane_Carousel_card.png', import.meta.url).href,
+    imageAlt: 'Earth shown with purple gas indicating atmospheric methane.'
+  },
+  {
     link: 'https://us-ghg-center.github.io/agu-2024-workshop/ghg-center-at-agu-2024.html',
-    title: 'Schedule: US GHG Center at AGU 2024',
+    title: 'US GHG Center at AGU 2024',
     description: 'Join us at the annual American Geophysical Union (AGU) meeting, held December 9-13 in Washington, D.C.',
     image: new URL('../../media/AGU2024_carousel_2.png', import.meta.url).href,
     imageAlt: 'US GHG Center at AGU 2024.'
@@ -10,14 +17,8 @@ export default [
 
     link: 'https://appliedsciences.nasa.gov/get-involved/training/english/arset-methane-observations-large-emission-event-detection-and',
     title: 'New Training on Methane Observations',
-    description: "Registration is open now on ARSET, NASA's remote sensing training program, for large emission event detection and monitoring.",
+    description: "ARSET, NASA’s remote sensing training program, recently held a training for large emission event detection and monitoring.  Event recording and materials are available online.",
     image: new URL('../../media/Methane_Website_Header.png', import.meta.url).href,
     imageAlt: 'Register: Stakeholder Engagement Forum.'
   },
-  {
-  link: '/stories/urban',
-  title: 'Spotlight on Urban Emissions',
-  description: 'The U.S. Greenhouse Gas Center offers an introduction to the latest national and city scale datasets and highlights innovative emissions research in cities across the country.',
-  image: 'https://www.nasa.gov/wp-content/uploads/2023/03/1-maia-los-angeles-1041.jpg',
-  imageAlt: 'Los Angeles skyline shown with visible haze of air pollution.'
-}]
+]
