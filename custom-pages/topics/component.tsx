6af0d75296454eef8f555f43ca6e2a3b99a3cfed
Slesa Adhikari <slesaad@gmail.com>
2024-11-21 09:44:21 -0600

    Refactor topics card to read from an object

--- custom-pages/topics/component.tsx
+++ custom-pages/topics/component.tsx
@@ -1,27 +1,17 @@
 import React from "$veda-ui/react";
-import { Link } from '$veda-ui/react-router-dom';
 import {
-  Card, 
-  CardHeader,
-  CardBody,
-  CardMedia,
-  CardGroup,
-  Grid,
   GridContainer,
 } from '$veda-ui/@trussworks/react-uswds';
-
-import {
-  CollecticonTextBlock,
-  CollecticonEnvelope,
-  CollecticonSpeechBalloon,
-} from '$veda-ui/@devseed-ui/collecticons';
 
 import { focusAreasStories } from "../../common/constants";
 import { Title } from "../../common/styled-components";
 
 import Keypoints from "../../common/keypoints";
+import Topic from "./components/Topic";
+import LearnMore from "../common/LearnMore";
 
 import { SUBSCRIPTION_URL } from "../../constants";
+import topics from "./topics";
 
 import '../../common/styles.scss';
 import './topics.scss';
@@ -31,41 +21,18 @@ export default function HomeComponent() {
     <div className="hug-reset-container margin-bottom-8">
       <GridContainer containerSize="widescreen">
         <Title>Content Collections by Topic</Title>
-        <CardGroup>
-          <Card layout="flagDefault" gridLayout={{tablet: {col: 10}, col: 12}} containerProps={{className:'border-0 position-relative card-shadow__hover'}}>
-            <CardMedia exdent>
-              <img width={"20em"} src={new URL('../../overrides/media/Methane_Carousel_card.png', import.meta.url).href} alt="earth shown with purple gas indicating atmospheric methane." />
-            </CardMedia>
 
-            <CardHeader>
-              <h3 className="usa-card__heading margin-top-4">Methane Emissions</h3>
-            </CardHeader>
-            <CardBody>
-              <p className="padding-bottom-2 margin-bottom-4">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages...
-              </p>
-            </CardBody>
-            <Link className="position-absolute top-0 left-0 width-full height-full blocklink" to="/stories/methane" />
-          </Card>
-        </CardGroup>
-
-        <CardGroup>
-          <Card layout="flagDefault" gridLayout={{tablet: {col: 10}, col: 12}} containerProps={{className:'border-0 position-relative card-shadow__hover'}}>
-            <CardMedia exdent>
-              <img src={new URL('./media/1-maia-los-angeles-1041.png', import.meta.url).href} alt="distant view of hazy cityscape." />
-            </CardMedia>
-
-            <CardHeader>
-              <h3 className="usa-card__heading margin-top-4">Urban Emissions</h3>
-            </CardHeader>
-            <CardBody>
-              <p className="padding-bottom-2 margin-bottom-4">
-              Cities and metropolitan regions, where most of the world's population live, are responsible for approximately 70% of greenhouse gas emissions. Researchers are making rapid advances in urban emissions measurement and modeling to provide robust, accurate, and reliable emissions estimates at fine scales. The U.S. Greenhouse Gas Center offers an introduction to new urban-relevant datasets and highlights innovative emissions research in cities across the country.
-              </p>
-            </CardBody>
-            <Link className="position-absolute top-0 left-0 width-full height-full blocklink" to="/stories/urban" />
-          </Card>
-        </CardGroup>
+        {
+          !!topics.length && topics.map(topic => (
+            <Topic
+              title={topic.title}
+              description={topic.description}
+              href={topic.href}
+              imgSrc={topic.img.src}
+              imgAlt={topic.img.alt}
+            />
+          ))
+        }
       
         <section>
           <Title>Core Science Focus Areas</Title>
@@ -73,33 +40,7 @@ export default function HomeComponent() {
           <Keypoints data={focusAreasStories} />
         </section>
 
-        <Grid tablet={{col:8, offset: 2}}>
-          <Title> Learn More and Share Your Ideas </Title>
-          <Grid row>
-              <Grid col='auto' className="display-flex flex-align-center margin-bottom-2">
-                  <CollecticonEnvelope size='xlarge' color='#082a64' className="grid-col-auto" />
-              </Grid>
-              <Grid col='fill' className="display-flex flex-align-center margin-bottom-2">
-                  <span className="margin-left-4 font-size-md-deprecated"> For the latest updates and information about the US GHG Center or to join a focus area group, <a href={SUBSCRIPTION_URL}>subscribe to our email updates list.</a></span>
-              </Grid>
-          </Grid>
-          <Grid row>
-              <Grid col='auto' className="display-flex flex-align-center margin-bottom-2">
-                  <CollecticonTextBlock size='xlarge' color='#082a64' className="grid-col-auto" />
-              </Grid>
-              <Grid col='fill' className="display-flex flex-align-center margin-bottom-2">
-                  <span className="margin-left-4 font-size-md-deprecated"> Read more about the US GHG Center news, trainings, and workshop opportunities on the <Link to="/news-and-events">News and Events page.</Link></span>
-              </Grid>
-          </Grid>
-          <Grid row>
-              <Grid col='auto' className="display-flex flex-align-center margin-bottom-2">
-                  <CollecticonSpeechBalloon size='xlarge' color='#082a64' className="grid-col-auto" />
-              </Grid>
-              <Grid col='fill' className="display-flex flex-align-center margin-bottom-2">
-                  <span className="margin-left-4 font-size-md-deprecated">  Do you have a US GHG Center portal suggestion or question? Reach the team using the "Contact Us" button at the top or bottom of every page.</span>
-              </Grid>
-          </Grid>
-        </Grid>
+        <LearnMore />
 
       </GridContainer>
     </div>
---
+++ custom-pages/topics/topics.js
@@ -0,0 +1,20 @@
+export default topics = [
+    {
+        "title": "Methane Emissions",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages...",
+        "img": {
+            "src": new URL('../../overrides/media/Methane_Carousel_card.png', import.meta.url).href,
+            "alt": "earth shown with purple gas indicating atmospheric methane."
+        },
+        "href": "/stories/methane",
+    },
+    {
+        "title": "Urban Emissions",
+        "description": "Cities and metropolitan regions, where most of the world's population live, are responsible for approximately 70% of greenhouse gas emissions. Researchers are making rapid advances in urban emissions measurement and modeling to provide robust, accurate, and reliable emissions estimates at fine scales. The U.S. Greenhouse Gas Center offers an introduction to new urban-relevant datasets and highlights innovative emissions research in cities across the country.",
+        "img": {
+            "src": new URL('./media/1-maia-los-angeles-1041.png', import.meta.url).href,
+            "alt": "distant view of hazy cityscape."
+        },
+        "href": "/stories/urban",
+    },
+]
