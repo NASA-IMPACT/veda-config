c212f457c9443a43f1bcacdba3955898eb399962
Slesa Adhikari <slesaad@gmail.com>
2024-11-22 10:42:37 -0600

    Update carousel text

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -2,7 +2,7 @@ export default [
   {
     link: '/stories/methane',
     title: 'Spotlight on Methane Emissions',
-    description: 'The U.S. Greenhouse Gas Center...',
+    description: 'The U.S. Greenhouse Gas Center offers a collection of tools, stories, training, and datasets to support greater understanding of methane emissions around the world.',
     image: new URL('../../media/Methane_Carousel_card.png', import.meta.url).href,
     imageAlt: 'Earth shown with purple gas indicating atmospheric methane.'
   },
@@ -17,7 +17,7 @@ export default [
 
     link: 'https://appliedsciences.nasa.gov/get-involved/training/english/arset-methane-observations-large-emission-event-detection-and',
     title: 'New Training on Methane Observations',
-    description: "ARSET, NASA’s remote sensing training program, recently held a training for large emission event detection and monitoring.  Event recording and materials are available online.",
+    description: "View training and materials from ARSET, NASA’s remote sensing training program, for large emission event detection and monitoring.",
     image: new URL('../../media/Methane_Website_Header.png', import.meta.url).href,
     imageAlt: 'Register: Stakeholder Engagement Forum.'
   },
