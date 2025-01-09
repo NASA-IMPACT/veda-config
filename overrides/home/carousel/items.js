9e9d0587e623098607bca5079042ab1c7e6a3eaf
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-01-09 09:56:55 -0600

    change order and add alt image text

--- overrides/home/carousel/items.js
+++ overrides/home/carousel/items.js
@@ -7,18 +7,18 @@ export default [
     imageAlt: 'Earth shown with purple gas indicating atmospheric methane.'
   },
   {
-    link: 'https://earth.gov/ghgcenter/stories/nist-methane-intercomparisons',
-    title: 'NIST Developing Standards for Methane Plume Data',
-    description: 'Description: Read about NIST efforts to develop consensus standards for growing volume of public and private methane plume satellite data.',
-    image: new URL('../../media/NIST_Methane_Intercomparisons_card.png', import.meta.url).href,
-    imageAlt: 'NIST Developing Standards for Methane Plume Data'
-  },
-  {
 
     link: 'https://appliedsciences.nasa.gov/get-involved/training/english/arset-methane-observations-large-emission-event-detection-and',
     title: 'New Training on Methane Observations',
     description: "View training and materials from ARSET, NASA’s remote sensing training program, for large emission event detection and monitoring.",
     image: new URL('../../media/Methane_Website_Header.png', import.meta.url).href,
     imageAlt: 'Register: Stakeholder Engagement Forum.'
+  },
+  {
+    link: 'https://earth.gov/ghgcenter/stories/nist-methane-intercomparisons',
+    title: 'NIST Developing Standards for Methane Plume Data',
+    description: 'Description: Read about NIST efforts to develop consensus standards for growing volume of public and private methane plume satellite data.',
+    image: new URL('../../media/NIST_Methane_Intercomparisons_card.png', import.meta.url).href,
+    imageAlt: 'Construction worker overlooking an industrial refinery at dusk, with interconnected glowing lines and nodes overlaid in the sky.'
   },
 ]
