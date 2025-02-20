f85ebd7ad0d5ee905f4ae589c4903ddea162d1d1
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-02-20 14:30:10 -0600

    add cookie and banner

--- veda.config.js
+++ veda.config.js
@@ -32,6 +32,20 @@ let subNavItems = [
   },
 ]
 
+const defaultGuidance = {
+  left: {
+    title: 'Official websites use .gov',
+    text: 'A **.gov** website belongs to an official government organization in the United States.',
+    iconAlt: 'Dot gov icon',
+    icon: '/img/icon-dot-gov.svg'
+  },
+  right: {
+    title: 'Secure .gov websites use HTTPS',
+    text: "A **lock icon** or **https://** means you've safely connected to the .gov website. Share sensitive information only on official, secure websites.",
+    iconAlt: 'HTTPS icon',
+    icon: '/img/icon-https.svg'
+  }
+};
 
 if (config.GOOGLE_FORM) {
   subNavItems = [
@@ -123,5 +137,37 @@ module.exports = {
   navItems: {
     mainNavItems: defaultMenuLinks,
     subNavItems: subNavItems
+  },
+  cookieConsentForm: {
+    title: 'Cookie Consent',
+    copy: 'We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our [Privacy Policy](https://www.nasa.gov/privacy/#cookies)',
+    theme: {
+      card: {
+        sideBarColor: '#082a64',
+        textColor: '#1B2631',
+        linkColor: '#175074'
+      },
+      acceptButton: {
+        default: { backgroundColor: '#082a64', textColor: 'white' },
+        hover: { backgroundColor: '#061A3A', textColor: '#white' }
+      },
+      declineButton: {
+        default: { borderColor: '#082a64', textColor: '#082a64' },
+        hover: { borderColor: '#061A3A', textColor: '#2c3e50' }
+      },
+      iconColor: { default: '#082a64', hover: '#061A3A' }
+    }
+  },
+  banner: {
+    headerText: 'An official website of the United States government',
+    headerActionText: "Here's how you know",
+    ariaLabel: 'Banner for official government website',
+    flagImgSrc: '/img/us_flag_small.png',
+    flagImgAlt: 'US flag',
+    leftGuidance: defaultGuidance.left,
+    rightGuidance: defaultGuidance.right,
+    className: '',
+    defaultIsOpen: false,
+    contentId: 'gov-banner-content'
   },
 };
