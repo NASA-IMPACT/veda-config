140aff4970dcdfef17c98e2af62ce73a18575504
Gjore Milevski <dzole0311@gmail.com>
2025-05-27 14:07:41 +0200

    chore: Update to prod endpoint for the features API

--- custom-pages/wildfire-visualization/component.tsx
+++ custom-pages/wildfire-visualization/component.tsx
@@ -9,7 +9,7 @@ export default function Component() {
     <div style={{ width: "100%", height: "100%" }}>
       <WildfireExplorer
         mapboxAccessToken={process.env.MAPBOX_TOKEN_FIRE_TOOL_STYLES}
-        featuresApiEndpoint={process.env.FEATURES_API_ENDPOINT_DEV}
+        featuresApiEndpoint={process.env.FEATURES_API_ENDPOINT_PROD}
       />
     </div>
   )
