f891b1be641eb7279562cd9180412f4a132981ee
Gjore Milevski <dzole0311@gmail.com>
2025-05-27 11:24:56 +0200

    feat: Set up the fire tool for publishing

--- custom-pages/wildfire-visualization/component.tsx
+++ custom-pages/wildfire-visualization/component.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+// @ts-ignore
+import { WildfireExplorer } from '@dsio/wildfire-explorer';
 import '@dsio/wildfire-explorer/dist/wildfire-explorer.css';
-
+import '../../styles/_custom.scss';
 
 export default function Component() {
 
   return (
-
     <div style={{ width: "100%", height: "100%" }}>
-      Hello from the custom page!
-      {/* <FireTool
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-      /> */}
+      <WildfireExplorer
+        mapboxAccessToken={process.env.MAPBOX_TOKEN_FIRE_TOOL_STYLES}
+        featuresApiEndpoint={process.env.FEATURES_API_ENDPOINT}
+      />
     </div>
   )
 }
